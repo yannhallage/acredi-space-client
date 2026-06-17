01983fa4dc7f7ae2fe8067c6ee2ece1c0d4481ad
AliceGraceYeo <alicegraceyeo.uta@gmail.com>
2026-06-17 16:38:11 +0000

    Update http.ts

--- src/shared/api/http.ts
+++ src/shared/api/http.ts
@@ -1,6 +1,7 @@
 import { clearAuthSession } from "./auth/session";
 
-const apiUrl = import.meta.env.VITE_API_URL;
+// const apiUrl = import.meta.env.VITE_API_URL;
+const apiUrl = "https://api-acredispace.acredigroup.com";
 
 const DEFAULT_API_BASE_URL = apiUrl || "http://localhost:8080/api";
 
